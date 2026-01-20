7e9885b43312b03a9fa5899519e2fd4e21fc9ad2
Ju XueShen <651606477@qq.com>
2026-01-20 07:54:15 -0800

    deadlock2

---
+++ day2/build/CMakeFiles/deadlock_detector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deadlock_detector.dir/src/deadlock_detector.cpp.o"
+  "CMakeFiles/deadlock_detector.dir/src/graph.cpp.o"
+  "libdeadlock_detector.pdb"
+  "libdeadlock_detector.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deadlock_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
